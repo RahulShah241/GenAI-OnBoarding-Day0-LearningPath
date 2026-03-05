cf50a0a7fe7c909db66753f0a2f1b777c11d7f6e
RahulShah241 <166903960+RahulShah241@users.noreply.github.com>
2026-03-06 00:46:45 +0530

    Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,28 +3,23 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
-    host: "::",
+    host: true,
     port: 8080,
     hmr: {
       overlay: false,
     },
-    // vite.config.js
-    server: {
-      host: true,
-      allowedHosts: [
-        "genai-onboarding-day0-learningpath-1.onrender.com"
-      ]
-    },
+    allowedHosts: [
+      "genai-onboarding-day0-learningpath-1.onrender.com"
+    ],
     proxy: {
       "/api": "http://localhost:8000",
     },
-
-
   },
+
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
